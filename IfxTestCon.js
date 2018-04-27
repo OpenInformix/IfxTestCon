49dc064dbab1b4d0b0d2fe9a76c46722708af0eb
Satyan <msatyan@hcl.com>
2018-04-27 14:38:53 -0500

    Connectivity Test

--- IfxTestCon.js
+++ IfxTestCon.js
@@ -1,5 +1,158 @@
-exports.BasicTest = function( )
+
+var format = require("string-template")
+var ifxobj = require('ifxnjs');
+
+
+
+
+class CBasicTest 
 {
- console.log( "BasicTest: Hello World!"  );
+    constructor() {  }
+
+    DirExec(dbCon, ErrIgn, sql)
+    {
+        try
+        {
+            var result = dbCon.querySync(sql);
+            console.log(sql);
+        }
+        catch (e)
+        {
+            console.log("--- " + sql);
+            if (ErrIgn != 1)
+            {
+                console.log(e);
+                console.log();
+            }
+        }
+    }
+
+    DoSomeWork(err, dbCon)
+    {
+        DirExec(dbCon, 1, "drop table t1");
+        DirExec(dbCon, 0, "create table t1 ( c1 int, c2 char(20) ) ");
+        DirExec(dbCon, 0, "insert into t1 values( 1, 'val-1' )");
+        DirExec(dbCon, 0, "insert into t1 values( 2, 'val-2' )");
+        DirExec(dbCon, 0, "insert into t1 values( 3, 'val-3' )");
+        DirExec(dbCon, 0, "insert into t1 values( 4, 'val-4' )");
+        DirExec(dbCon, 0, "insert into t1 values( 5, 'val-5' )");
+
+        console.log();
+        console.log(" --- SELECT * FROM t1 ------ ");
+
+        var rows = dbCon.querySync("SELECT * FROM t1");
+        console.log();
+        console.log(rows);
+    };
+
+    RunTest1(ConStr, drda=false)
+    {
+
+        console.log(" --- Executing SqliBasicTest ....");
+        var dbCon;
+        try
+        {
+            if( drda )
+            {
+                // This is for DRDA
+                dbCon = null;
+            }
+            else
+            {
+                dbCon = ifxobj.openSync(ConStr);
+            }
+        }
+        catch (e)
+        {
+            console.log(e);
+            return;
+        }
+
+        DoSomeWork(dbCon);
+
+        try
+        {
+            dbCon.closeSync();
+        }
+        catch (e)
+        {
+            console.log(e);
+        }
+        console.log(" --- End ifxnjs.openSync()");
+    }
+
 }
 
+
+
+
+function main()
+{
+
+
+    var mTest = new CBasicTest();
+
+    conInfo =
+    {
+        myHost : "ec2-52-207-237-201.compute-1.amazonaws.com",
+        myServer :  "ol_aws",
+        myDb: "db1",
+        myUser: "dbuser1",
+        myPwd: "mypwd123",
+
+        //////////////////
+        sqliPort: 9088,
+        sslPort:9089,
+        sqliProto: "onsoctcp",
+        sslProto: "olsocssl"
+    };
+
+
+    ////////////////////////////////////////////////
+const Connectivitys = {
+    SQLI: 'sqli',
+    SQLI_SSL: 'sqli_ssl',
+    DRDA: 'drda',
+    DRDA_SSL: 'drda_ssl'
+}
+
+
+
+
+    ////////////////////////////////////////////////
+
+    var ConnStr  =  "SERVER=ids0;DATABASE=db1;HOST=127.0.0.1;SERVICE=9088;PROTOCOL=onsoctcp;UID=informix;PWD=xxxx;";
+    var sslConStr = 'HOST={myHost};SERVER={myServer}; SERVICE={sslPort}; PROTOCOL={sslProto}; DATABASE={myDb};UID={myUser};PWD={myPwd};CLIENT_LOCALE=en_us.8859-1;DB_LOCALE=en_us.utf8';
+    var sqliConStr = 'HOST={myHost};SERVER={myServer};SERVICE={sqliPort};PROTOCOL={sqliProto};DATABASE={myDb};UID={myUser};PWD={myPwd};CLIENT_LOCALE=en_us.8859-1;DB_LOCALE=en_us.utf8';
+
+
+    for (let item in Connectivitys)
+    {
+        let ConnType = Connectivitys[item];
+
+        switch (ConnType)
+        {
+            case Connectivitys.SQLI:
+                ConnStr = format(sqliConStr, conInfo);
+                break;
+
+            case Connectivitys.SQLI_SSL:
+                ConnStr = format(sslConStr, conInfo);
+                break;
+
+
+            case Connectivitys.DRDA:
+                ConnStr = " Connection string for DRDA"
+                break;
+
+            case Connectivitys.DRDA_SSL:
+                ConnStr = " Connection string for DRDA SSL"
+                break;
+        }
+        console.log(ConnStr);
+        //mTest.RunTest1(SqliConStr);
+    }
+
+}
+
+main();
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # IfxTestCon
 Basic Connectivity Test with Informix Database
+
+
+### DB Server Setup
+```
+sudo -u informix bash
+dbaccess 
+
+
+CREATE   database   db1  with log;
+
+create user dbuser1 with password 'mypwd123' properties USER nobody authorization (DBSA);
+
+GRANT CONNECT TO dbuser1;
+GRANT DBA     TO dbuser1;
+```
+
+
